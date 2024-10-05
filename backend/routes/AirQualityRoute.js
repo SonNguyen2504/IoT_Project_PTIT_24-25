d55f7efc147312287adf98efe1d36b7ccd4a97f5
Son Nguyen <trgson250411@gmail.com>
2024-10-05 21:54:13 +0700

    update routes

--- backend/routes/AirQualityRoute.js
+++ backend/routes/AirQualityRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAirQuality } = require('../controllers/AirQualityController');
+const { getAirQuality, getAllAirQuality } = require('../controllers/AirQualityController');
 
 router.route('/air-quality')
     .get(getAirQuality);
+router.route('/all-air-quality')
+    .get(getAllAirQuality);
 
 module.exports = router;
--- backend/routes/DeviceStatusRoute.js
+++ backend/routes/DeviceStatusRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getDeviceStatus } = require('../controllers/DeviceStatusController');
+const { getDeviceStatus, getAllDeviceStatus } = require('../controllers/DeviceStatusController');
 
 router.route('/device-status').get(getDeviceStatus);
+router.route('/all-device-status').get(getAllDeviceStatus);
 
 module.exports = router;
